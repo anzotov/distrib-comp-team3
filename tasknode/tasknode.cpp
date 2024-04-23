f92e818bb9e9fe8a5fad8495fb8b50e5a86473a2
Andrey Zotov <Andrey.N.Zotov@yandex.ru>
2024-04-23 20:49:33 +0500

    TaskNode: парсинг входного файла и открытие выходного

--- tasknode/tasknode.cpp
+++ tasknode/tasknode.cpp
@@ -1,7 +1,72 @@
 #include "tasknode.h"
 #include <QtDebug>
+#include <QFile>
 
-TaskNode::TaskNode(QObject *parent, const QString ip, const QString port, const QString input, const QString output)
+TaskNode::TaskNode(QObject *parent, const QString &ip, const QString &port, const QString &input, const QString &output) : QObject(parent)
 {
-    qDebug() << "TaskNode(" << ip << port << input << output;
+    qDebug() << "TaskNode(" << ip << "," << port << "," << input << "," << output << ")";
+
+    parseInput(input);
+
+    if (output == "-")
+    {
+        m_outputSteam = new QTextStream(stdout, QIODevice::WriteOnly);
+    }
+    else
+    {
+        auto outputFile = new QFile(output, this);
+        if (!outputFile->open(QIODevice::WriteOnly | QIODevice::Text))
+        {
+            qFatal("Error opening output file %s", qPrintable(output));
+        }
+        m_outputSteam = new QTextStream(outputFile);
+    }
+}
+
+TaskNode::~TaskNode()
+{
+    qDebug() << "~TaskNode()";
+    delete m_calcTask;
+    delete m_outputSteam;
+}
+
+void TaskNode::parseInput(const QString &input)
+{
+    QFile inputFile(input);
+    if (input == "-")
+    {
+        if (!inputFile.open(stdin, QIODevice::ReadOnly | QIODevice::Text))
+        {
+            qFatal("Error opening standard input");
+        }
+    }
+    else
+    {
+        if (!inputFile.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            qFatal("Error opening input file %s", qPrintable(input));
+        }
+    }
+    QTextStream inputStream(&inputFile);
+
+    m_calcTask = new CalcTask();
+    QString line;
+    bool firstString = true;
+    while (inputStream.readLineInto(&line) && !line.isEmpty())
+    {
+        if (firstString)
+        {
+            m_calcTask->function = line;
+            firstString = false;
+            continue;
+        }
+        m_calcTask->data << line;
+    }
+    qDebug() << "m_calcTask->function: " << m_calcTask->function;
+    qDebug() << "m_calcTask->data: " << m_calcTask->data;
+
+    if (m_calcTask->function.isEmpty() || m_calcTask->data.isEmpty())
+    {
+        qFatal("Incorrect input data!");
+    }
 }
--- tasknode/tasknode.h
+++ tasknode/tasknode.h
@@ -1,13 +1,22 @@
 #pragma once
 
+#include "../common/calcTask.h"
+
 #include <QObject>
 #include <QString>
+#include <QTextStream>
 
-class TaskNode : public QObject
+class TaskNode final : public QObject
 {
     Q_OBJECT
 
 public:
     TaskNode(QObject *parent,
-             const QString ip, const QString port, const QString input, const QString output);
+             const QString &ip, const QString &port, const QString &input, const QString &output);
+    ~TaskNode();
+
+private:
+    void parseInput(const QString &input);
+    CalcTask *m_calcTask = nullptr;
+    QTextStream *m_outputSteam = nullptr;
 };
