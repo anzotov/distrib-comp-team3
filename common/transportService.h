9b7d49b1a72c281ce9caf240858581b4765b303b
Emil Minnulin <e.r.minnulin@yandex.ru>
2024-04-23 22:31:45 +0500

    TransportService: доработка структуры класса

--- common/transportService.h
+++ common/transportService.h
@@ -1,28 +1,29 @@
 #pragma once
 
+#include "../common/calcTask.h"
 #include <QObject>
 #include <QtNetwork/QTcpSocket>
 #include <QtNetwork/QTcpServer>
 #include <QtNetwork/QHostAddress>
 #include <QString>
-#include <calcTask.h>
+#include <QPointer>
 
-class transportService : public QObject
+class TransportService : public QObject
 {
     Q_OBJECT
 public:
-    explicit transportService(QObject *parent = nullptr);
-
-
+    explicit TransportService(QObject *parent = nullptr) : QObject(parent);
 signals:
-    void newConnection(const QHostAddress IP, const quint64 port);
+    void newConnection(const QHostAddress IP, const quint64 port, const quint64 socketNum);
     void receiveMainTask(const CalcTask& task, const quint64 socketNum);
     void receiveResultTask(const QStringList& result, const quint64 socketNum);
 public slots:
     void openConnection(const QHostAddress IP, const quint64 port);
-    void closeConnection(const QHostAddress IP, const quint64 port);
+    void closeConnection(const quint64 socketNum);
     void sendMainTask(const CalcTask& task, const quint64 socketNum);
     void sendMainResult(const QStringList& result, const quint64 socketNum);
+    void startListening(const quint64 port);
+    void stopListening(const quint64 port);
 private:
     QTcpServer m_server;
     QVector<QPointer<QTcpSocket>> m_socket;
